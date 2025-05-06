a0bfa28e54986c435c6c27421337501fa7c61bb8
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-05-05 21:53:53 -0400

    gamestate, reference ids

--- Include/entity.h
+++ Include/entity.h
@@ -5,47 +5,99 @@ typedef enum EntityType {
     ENTITY_TYPE_NONE,
     ENTITY_TYPE_PLAYER,
     ENTITY_TYPE_DRAGON,
+    ENTITY_TYPE_PRESSURE_PLATE,
+    ENTITY_TYPE_DOOR,
+    ENTITY_TYPE_LEVER,
+    ENTITY_TYPE_TRAP,
+    ENTITY_TYPE_SEQUENCE_CONTROLLER,
 } EntityType;
 
 typedef enum StatusEffect {
-    STATUS_ON_FIRE = 0x1,
-    STATUS_POISONED = 0x2,
-    STATUS_ROOTED = 0x4,
-    STATUS_INVINCIBLE = 0x8,
-    STATUS_STUNNED = 0x10,
-    STATUS_STARVING = 0x20,
+    STATUS_ON_FIRE     = 0x01,
+    STATUS_POISONED    = 0x02,
+    STATUS_ROOTED      = 0x04,
+    STATUS_INVINCIBLE  = 0x08,
+    STATUS_STUNNED     = 0x10,
+    STATUS_STARVING    = 0x20,
+} StatusEffect;
+
+typedef enum InteractionType {
+    INTERACTION_NONE,
+    INTERACTION_TOGGLE,     // On/off switch
+    INTERACTION_MOMENTARY,  // Active while pressed
+    INTERACTION_PERMANENT,  // Stays active once triggered
+    INTERACTION_SEQUENCE,   // Must be triggered in correct order
+} InteractionType;
+
+typedef enum IteractionState {
+    INTERACTION_INACTIVE,
+    INTERACTION_ACTIVE,
+    INTERACTION_COOLDOWN,
+} IteractionState;
+
+typedef enum ActivationRule {
+    ACTIVATION_RULE_ANY,       // Any trigger activates
+    ACTIVATION_RULE_ALL,       // All triggers must be active
+    ACTIVATION_RULE_SEQUENCE,  // Must be activated in a specific sequence
+} ActivationRule;
+
+typedef struct {
+    u32 fire_damage;
+    u32 physical_damage;
+    u32 cold_damage;
+} Damage;
+
+typedef struct {
+    u32 flags;
+    u32 duration_in_seconds;
 } StatusEffect;
 
 typedef struct {
-    // Base
-    u32 id;
+    u32 max_health;
+    u32 health;
+
+    u32 max_stamina;
+    u32 stamina;
+
+    u32 max_mana;
+    u32 mana;
+} Resource;
+
+typedef struct Interactable {
+    bool is_interactable;
+    InteractionType type;
+    IteractionState state;
+    float active_timer;
+    float cooldown_timer;
+
+    // Sequence logic
+    s32 true_sequence_value; // default set to -1 as placeholder
+    s32 sequence_order; // default set to -1 as placeholder then overwriten by true_sequence_value when trigger is active.
+} Interactable;
+
+typedef struct {
+    bool is_triggerable;
+    bool active;
+    ActivationRule activation_rule;
+
+    Entity** interactables; // for sequence interate through check if first non-negative number is 0 then 0 + 1 and so on
+} Triggerable;
+
+typedef struct Entity {
+    // base
+    u64 unique_reference_id;
+    u32 entity_index;
     EntityType type;
-    float x;
-    float y;
-    u32 color_texture_id;
-    u32 normal_map_id;
-    u32 height_map_id;
-    // Shader shader;
+    Transform transform;
+    Mesh mesh;
+    Material material;
 
-    struct { // Damage
-        u32 fire_damage;
-        u32 physical_damage;
-        u32 cold_damage;
-    };
+    // gameplay
+    Damage damage;
+    StatusEffect status_effect;
+    Resource resource;
 
-    struct { // Status Effects
-        u32 status_flags;
-        u32 duration_in_seconds;
-    };
-
-    struct { // Resources
-        u32 max_health;
-        u32 health;
-
-        u32 max_stamina;
-        u32 stamina;
-
-        u32 max_mana;
-        u32 mana;
-    };
+    // puzzles
+    Interactable interactable;
+    Triggerable triggerable;
 } Entity;
--- Source/game_state.c
+++ Source/game_state.c
@@ -4,6 +4,35 @@ typedef struct GameState {
     u8* permanent_memory;
     u8* temperary_memory;
 
+    struct {
+        float gravity;    // defaults to -9.8
+        float time_scale; // defaults to  1.0
+    } rules;
+
+    u64 next_unique_reference_id; // stable and unique
     Entity gameplay_entities[256];
     Entity static_entities[256];
 } GameState;
+
+internal GameState deserialize_game_state() {
+    GameState game = {0};
+    game.rules.gravity = -9.8;
+    game.rules.time_scale = 1.0;
+
+    // if there does not exist a serialized file
+    // just defaults
+
+    return game;
+}
+
+GameState game_state_restore(u8* permanent_memory, u8* temperary_memory) {
+    GameState game = deserialize_game_state();
+
+    // Date: May 05, 2025
+    // NOTE(Jovanni): I have to overwrite these becuase the serialized data
+    // is not longer useful after the virtual address space changes on a new instance of the process
+    game.permanent_memory = permanent_memory;
+    game.temperary_memory = temperary_memory;
+
+    return game;
+}
