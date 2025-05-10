3a2f7176b4561ff3a1e525afece53050238af42b
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-05-10 10:07:02 -0400

    interesting

--- Source/game_state.c
+++ Source/game_state.c
@@ -30,11 +30,11 @@ GameState game_state_restore(u8* memory, size_t memory_size_bytes) {
     // Partition memory manually
     size_t arena_size = memory_size_bytes / 5; // Divide equally among 5 arenas
 
-    game.permanent_arena  = ckg_arena_create_fixed(memory + arena_size * 0, arena_size);
-    game.entity_arena     = ckg_arena_create_fixed(memory + arena_size * 1, arena_size);
-    game.asset_arena      = ckg_arena_create_fixed(memory + arena_size * 2, arena_size);
-    game.evictable_arena  = ckg_arena_create_fixed(memory + arena_size * 3, arena_size);
-    game.scratch_arena    = ckg_arena_create_fixed(memory + arena_size * 4, arena_size);
+    game.permanent_arena  = ckg_arena_create_fixed(memory + (arena_size * 0), arena_size);
+    game.entity_arena     = ckg_arena_create_fixed(memory + (arena_size * 1), arena_size);
+    game.asset_arena      = ckg_arena_create_fixed(memory + (arena_size * 2), arena_size);
+    game.evictable_arena  = ckg_arena_create_fixed(memory + (arena_size * 3), arena_size);
+    game.scratch_arena    = ckg_arena_create_fixed(memory + (arena_size * 4), arena_size);
 
     return game;
 }
